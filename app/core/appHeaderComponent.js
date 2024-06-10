3bf6a749bbe7fefad107f1d8e19106dda49c6ebb
JCampo1502 <javierdavidcamposuarez15@gmail.com>
2024-06-09 22:50:06 -0500

    style: :lipstick: Creating menu

--- app/core/appHeaderComponent.js
+++ app/core/appHeaderComponent.js
@@ -13,26 +13,27 @@ export class AppHeaderComponent extends HTMLElement{
     }
 
     static get #cssTemplateStyles(){
-        return /* css */`            
-            :host{
-                display:block;                
-                height: 4rem;
-                background:var(--background);
-                display:flex;
-                align-items:center;
-                box-shadow: var(--box-shadow-sm);
-                padding-inline:var(--padding-inline);
-                padding-block:var(--padding-block);
+        return /* css */`                       
+        
+            :host{                           
+                display:block;                   
                 position:sticky;
                 top:0;
                 z-index:var(--zindex-sticky);
             }
 
             .header{
-                width:100%;
+                display:flex;
+                align-items:center;
+                box-shadow: var(--box-shadow-sm);
+                padding-inline:var(--padding-inline);
+                padding-block:var(--padding-block);
+                background:red;
+                height: 4rem;
+                width:calc(85vw - 2rem);
                 display: flex;
                 align-items: center;
-                justify-content: start;
+                justify-content: space-between;
             }
 
             .header__menu{
@@ -40,7 +41,8 @@ export class AppHeaderComponent extends HTMLElement{
                 height:30px;
                 background:var(--background);
                 border:none;   
-                padding:0;             
+                padding:0;       
+                order: 1;      
             }
 
             .header__icon{
@@ -51,6 +53,30 @@ export class AppHeaderComponent extends HTMLElement{
             .header__logo{
                 width: 92px;
                 height:50px;
+            }
+
+            .header__nav{
+                background:var(--background);  
+                height: calc(100vh - 5rem);
+                width:85vw;                
+                position:absolute;
+                top:5rem;
+                left:0;                
+            }
+
+            .header__nav::before{
+                content:"";
+                position:absolute;
+                top: -5rem;
+                left: 0;
+                height:100vh;
+                width:100vw;
+                background:#000000ab;
+                z-index:-1;
+            }
+
+            .header__nav > *{
+                background:red;
             }
             
         `;
@@ -63,34 +89,34 @@ export class AppHeaderComponent extends HTMLElement{
             <button class="header__menu">
                 <img src="app/assets/menu.svg" alt="menu" class="header__icon">        
             </button>
-            <img src="images/AgileInovaLogo.png" alt="logo" class="header__logo">        
+            <img src="images/AgileInovaLogo.png" alt="logo" class="header__logo">
+            <nav class="header__nav">
+                <h4 class="nav__title">Javascript</h4>
+                <ul class="nav__list">
+                    <li class="nav__item">
+                        <a href="#" class="nav__link nav__link--sublist">
+                            Conceptos Basicos
+                        </a>
+                        <ul class="nav__list nav__list--sblist">
+                            <li class="list__item">
+                                <a href="#" class="list__link">Ejercicio 01</a>
+                            </li>
+                            <li class="list__item">
+                                <a href="#" class="list__link">Ejercicio 02</a>
+                            </li>
+                            <li class="list__item">
+                                <a href="#" class="list__link">Ejercicio 03</a>
+                            </li>
+                            <li class="list__item">
+                                <a href="#" class="list__link">Ejercicio 04</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
         </header>`;
     }
 }
 
 customElements.define("app-header",AppHeaderComponent );
 
-/* <nav class="nav">
-            <h4 class="nav__title">Javascript</h4>
-            <ul class="nav__list">
-                <li class="nav__item">
-                    <a href="#" class="nav__link nav__link--sublist">
-                        Conceptos Basicos
-                    </a>
-                    <ul class="nav__list nav__list--sblist">
-                        <li class="list__item">
-                            <a href="#" class="list__link">Ejercicio 01</a>
-                        </li>
-                        <li class="list__item">
-                            <a href="#" class="list__link">Ejercicio 02</a>
-                        </li>
-                        <li class="list__item">
-                            <a href="#" class="list__link">Ejercicio 03</a>
-                        </li>
-                        <li class="list__item">
-                            <a href="#" class="list__link">Ejercicio 04</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </nav> */
